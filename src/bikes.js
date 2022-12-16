a51ef1c841e210e030659e5fcfb4b7fe5cc8c86c
Barso2 <reznitskyalex@gmail.com>
2022-12-16 23:56:14 +0200

    add some changes to 1000px

--- src/bikes.js
+++ src/bikes.js
@@ -1,20 +1,17 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-
-const detailsSound = document.querySelector("#audio");/********** */
-
+const detailsSound = document.querySelector("#audio");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
-const detailsContainer = document.querySelector(".details-container")/****/
+const detailsContainer = document.querySelector(".details-container")
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.querySelector("#hide-button");
 const HIDDEN = 'hidden';
-const IS_POINT = "is-point"/****/
+const IS_POINT = "is-point"
 function showDetails(){
     mainElement.classList.remove("hidden");
-    detailsContainer.classList.add(IS_POINT)/** */
+    detailsContainer.classList.add(IS_POINT)
     setTimeout(function(){
-        detailsContainer.classList.remove(IS_POINT)/** */
-    
+        detailsContainer.classList.remove(IS_POINT)
     })
 }
 function hideDetails(){
@@ -23,10 +20,9 @@ function hideDetails(){
 function setDetails(anchor){
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
-    detailsSound.src = anchor.getAttribute("data-details-sound");/*<<<<<<<<**********>>>>>>>> */
+    detailsSound.src = anchor.getAttribute("data-details-sound");
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
     showDetails()
-    
 }
 for(let i = 0; i < anchorElements.length; i++){
     anchorElements[i].addEventListener("click", function(){
