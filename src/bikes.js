8312892a23b6771fc5d186662b5884aaf5e4732d
Barso2 <reznitskyalex@gmail.com>
2022-12-16 19:44:48 +0200

    added sounds + animation

--- src/bikes.js
+++ src/bikes.js
@@ -1,12 +1,21 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 
+const detailsSound = document.querySelector("#audio");/********** */
+
+const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const detailsContainer = document.querySelector(".details-container")/****/
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.querySelector("#hide-button");
 const HIDDEN = 'hidden';
+const IS_POINT = "is-point"/****/
 function showDetails(){
     mainElement.classList.remove("hidden");
+    detailsContainer.classList.add(IS_POINT)/** */
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT)/** */
+    
+    })
 }
 function hideDetails(){
     mainElement.classList.add(HIDDEN)
@@ -14,8 +23,10 @@ function hideDetails(){
 function setDetails(anchor){
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
+    detailsSound.src = anchor.getAttribute("data-details-sound");/*<<<<<<<<**********>>>>>>>> */
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
     showDetails()
+    
 }
 for(let i = 0; i < anchorElements.length; i++){
     anchorElements[i].addEventListener("click", function(){
