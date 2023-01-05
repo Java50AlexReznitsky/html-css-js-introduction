e4e436e3f4d8902312b119dae38bbe5610c736c5
Barso2 <reznitskyalex@gmail.com>
2023-01-06 00:01:28 +0200

    learned splice and slice functions

--- app.js
+++ app.js
@@ -1,57 +1,66 @@
-// const ar = [];
-// const ar1 = [1,2,3,4]
-// // .push - adds elements to array
-// ar.push(...ar1); //... = spread elements of ar1 in ar
-// ar.push('abc');
-function getRandomNumber(min, max){
-    min = Math.trunc(min);
-    max = Math.trunc(max);
-    return min + Math.trunc(Math.random() * (max - min + 1));
+const array = [10,20,30,-10,11,100];
+// array.splice(2,2);
+//                      false
+//                  true == false
+//                 1 < 5
+//              true < 5
+// console.log(3 < 4 < 5 == false)
+// const index = array.indexOf(30);
+// array.splice(index, 1); // 1 is a number of deleted elements from array
+// array.splice(index); //  without the second parameter, splice will delete the rest of array, starting from index
+// array.splice(index, undefined, -1000);// removes all, and adds -1000 to array ->> [-1000]
+// const res = array.splice(index, 1); // keeps the deleted element in variable - res 
+// array.splice(index, 0, -10, -20); // 0 - nothing will be deleted, -10, -20 will be inserted after index
+// replace
+// array.splice(index, 2,20);// replaces 
+// array.splice(index, 1, ...ar1);
 
+//slice
+
+// const numbersPerPage = 2;
+// const page = 2;
+// const firstIndex = (page - 1) * numbersPerPage
+// const pageNumbers = array.slice(firstIndex, firstIndex + numbersPerPage);
+// const arr1 = array.map(multiply2);
+// function multiply2(number){
+//     return number * 2;
+// }
+// const arr2 = array.map(function(number, index){
+//     return index + 1 + '. ' + number;
+// })
+// const arr3 = array.map(function(number){
+//     return `<li>${number}</li>`;
+// })
+// const ulElement = `<ul>${arr3.join('')}</ul>`;
+
+//HW #14
+//1.
+function coloringString(str1, str2){
+    //str1 - string
+    //str2 - string
+    // the string should be with the same length
+    //returns array of following colors
+    //if str2[i] exists in str1 but in another position - i-th color yellow
+    //if str2[i] == str1[i] -i-th color green
+    //if str2[i] doesn't exist in str1 - i-th color red
+    //For getting array from string, there is the following expression Array.from(string);
+    //Examples: coloringString("pappy", "apple") => ["yellow", "yellow","green","red","red"]
+    //coloringString("pappy", "pappy") => ["green","green","green","green","green"]
 }
-function getRandomMatrix(rows, columns, min, max){
-    const matrix = [];
-    for(let i = 0; i < rows; i++){
-        matrix.push([]);
-        for(let j = 0; j < columns; j++){
-            matrix[i].push(getRandomNumber(min, max))
-        }
-    }
-    return matrix;
-}//                         rows colmn min max  
-// const matrix = getRandomMatrix(3, 4, 0, 1);
-const ar10 = [1,2,3,4,5];
-const str = ar10.join('_') // by defaults the delimeter is: ","
-//HTML
-//<ul class="list_class">
-//  <li class="item_class">
-//      <div class="white" || "black"></div>
-//  </li>
-//  ...............
-//</ul>
-//<ul class="list_class">
-//  <li class="item_class">
-//      <div class="black" || "black"></div>
-//  </li>
-//  ...............
-//</ul>
-function getHtmlUl(array){
-    //TODO
-    return htmlString;
-} 
-const strClass = getRandomNumber(0,1) == 0? 'white': 'black';
-const str1 = 'hello world';
-const str2 = 'hello' + '"world"'
-const str3 = `class="${strClass}"`;
-function matrixTransp(matrix){
-    //TODO
-    //
-    //returns transp matrix
-    //matrix = [1,2], //input
-    //         [3,4],
-    //         [4,5]
-    //         ]
-    //
-    // output[1,3,4]
-    //       [2,4,5]
+//getting array of numbers in a given range Example as follows
+const min = 5;
+const max = 15;
+const ar4 = array.filter(function(number){
+    return number >= min && number <= max;
+})
+//getting new array of the elements of the source array with even index
+const ar5 = array.filter(function(__, index){
+    return index % 2 === 0;
+})
+//2.
+function getNumbersWithDigitsAmount(digitsAmount, array){
+    // takes digitsAmount - given number of digits; array - any array of numbers
+    // return array of only numbers from the given array comprising of the given number
+    //examples: getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000]) returns [100, -100]
 }
+
