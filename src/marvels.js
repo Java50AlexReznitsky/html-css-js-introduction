50a460e8e07501d83510918a954845904c9fac7b
Barso2 <reznitskyalex@gmail.com>
2023-01-16 00:10:14 +0200

    HW-17 solution updated

--- src/marvels.js
+++ src/marvels.js
@@ -1,45 +1,98 @@
-//references to HTML
+// references to HTML elements
 const questionElement = document.getElementById("question");
 const wordInputElement = document.getElementById("word-input");
-const remainLettersElement = document.getElementById("letters-remained");
+const remainedLettersElement = document.getElementById("letters-remained");
 const letterInputElement = document.getElementById("letter-input");
 const wordElement = document.querySelector(".word");
 const resultMessage = document.getElementById("result-message");
 const playAgain = document.getElementById("play-again");
 let wordLettersElement;
-//global variables
-const questionWords = [["question1", "answer1"], ["question2", "answer2"]];
+
+// global variables                                         
+const questionsWords = [
+  ['Which big cat is the largest?', 'tiger'],
+  ['Which is the largest planet in the solar system?', 'jupiter'],
+  ['In which city did the Olympic games originate?', 'greece'],
+  ['How many Olympic rings are there?', 'five'],
+  ['What is the fastest aquatic animal?', 'sailfish'],
+  ['Are worker bees male or female?', 'female'],
+  ['Which color is an emerald?', 'green'],
+  ['Whose nose grew longer every time he lied?', 'pinocchio'],
+  ['Which US state is famous for Hollywood?', 'california']
+];
+
 let currentIndex = 0;
-let initialLetterNumbers;
-let remainedLetterNumber;
-//functions
+let initialLettersNumber;
+let remainedLettersNumber;
+let questionAnswer;
+// functions
 function startGame() {
-  wordInputElement.value = '';
+  wordInputElement.value = "";
   wordInputElement.readOnly = true;
-  questionElement.innerHTML = questionWords[currentIndex][0]
-  wordElement.innerHTML = fillLettersDivs();//
-  playAgain.style.display = "none"
-  resultMessage.innerHTML = '';
-  wordElement.innerHTML = getWordDivs;
+  letterInputElement.readOnly = false;
+  questionAnswer = getWord();
+  questionElement.innerHTML = questionAnswer[0]
+  playAgain.style.display = "none";
+  resultMessage.innerHTML = "";
+  wordElement.innerHTML = getWordDivs();
+  initialLettersNumber = Math.ceil(questionAnswer[1].length * 0.3);
+  remainedLettersElement.innerHTML = `${initialLettersNumber} letter(s) to guess`;
+  remainedLettersNumber = initialLettersNumber;
+
 }
-function fillLettersDivs(){
-  const arrayWord = Array.from(questionWords[currentIndex][1]);
-  wordElement.innerHTML = arrayWord.reduce(function(res,cur){
-    res = res + '<div class="letter"></div>'
-        return res;
-  },'');
+function getWord() {
+  const index = Math.trunc(Math.random() * questionsWords.length);
+  return questionsWords[index];
 }
+function getWordDivs() {
+  let wordDivs = "";
+  for (let i = 0; i < questionAnswer[1].length; i++) {
+    wordDivs += `<div class="letter" id="letter${i}">${questionAnswer[1][i]}</div>`;
+  }
+  return wordDivs;
+}
+
 function checkWord() {
-  //TODO
+  if (wordInputElement.value === questionAnswer[1]) {
+    finishGame("Congratulations, you guessed the word!");
+    for (let i = 0; i < questionAnswer[1].length; i++) {
+      document.getElementById(`letter${i}`).style.backgroundColor = "white";
+    }
+  } else {
+    finishGame("Sorry, the word is incorrect.");
+  }
 }
+
 function processLetter() {
-  //TODO
+  if (remainedLettersNumber > 0) {
+    remainedLettersNumber--;
+    remainedLettersElement.innerHTML = `${remainedLettersNumber} letter(s) to guess`;
+    for (let i = 0; i < questionAnswer[1].length; i++) {
+      if (letterInputElement.value === questionAnswer[1][i]) {
+        document.getElementById(`letter${i}`).style.backgroundColor = "white";
+      }
+    }
+    letterInputElement.value = "";
+    if (remainedLettersNumber === 0) {
+      remainedLettersElement.innerHTML = 'Your attempts to guess the letter are over, guess the word!'
+      wordInputElement.readOnly = false;
+      letterInputElement.readOnly = true;
+    }
+  }
 }
+
 function takeChance() {
-  //TODO
+  wordInputElement.readOnly = false;
+  letterInputElement.readOnly = true;
 }
-function finishGame() {
-  //TODO
+
+function finishGame(message) {
+  resultMessage.innerHTML = message;
+  playAgain.style.display = "block";
+  wordInputElement.readOnly = true;
+  letterInputElement.readOnly = true;
+  for (let i = 0; i < questionAnswer[1].length; i++) {
+    document.getElementById(`letter${i}`).style.backgroundColor = "white";
+  }
 }
 startGame();
-//actions
