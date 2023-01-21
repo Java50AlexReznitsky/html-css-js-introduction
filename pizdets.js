8f6630401d452980b0c18ba80a3c630c7b2f141f
Barso2 <reznitskyalex@gmail.com>
2023-01-22 01:20:00 +0200

    HW-a18 solution

--- pizdets.js
+++
@@ -1,6 +0,0 @@
-function getMostPopulatedCountry(employees) {
-    return (Object.entries(employees.reduce((acc, curr) => {
-        acc[curr.address.country] = (acc[curr.address.country] || 0) + 1;
-        return acc;
-    }, {})).sort((entry1, entry2) => entry2[1] - entry1[1]))[0][0];
-}
