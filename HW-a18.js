3e745d936b56e7be384cbeff2d736574486b9eab
Barso2 <reznitskyalex@gmail.com>
2023-01-23 21:32:26 +0200

    updated HW-a18

--- HW-a18.js
+++ HW-a18.js
@@ -6,16 +6,16 @@ function createPerson(id, name, address) {
 }
 const persons = [
     createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
-    createPerson(124, "Olya", createAddress("Rehovot", "Pr.Plaut")),
     createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+    createPerson(124, "Olya", createAddress("Rehovot", "Pr.Plaut")),
     createPerson(126, "Sara", createAddress("Lod", "Sokolov"))
 ]
 
 //=======================================const maxIdRehovotResident = 
 // 1 ) Calculate IN ONE LINE OF CODE the name of Person living in Rehovot and having maximal value of 'id'
 //     The expected result: Olya
-persons.filter(p => p.address.city === "Rehovot").reduce((prev, curr) => (prev.id > curr.id) ? prev : curr).name;
+console.log(persons.filter(pers => pers.address.city === "Rehovot").reduce((prev, curr) => (prev.id > curr.id) ? prev : curr).name);
 //=======================================
 // 2*) Build IN ONE LINE OF CODE the statistics of persons amount per city.
 //     The expected result is object: {Rehovot:2, 'Tel-Aviv':1,Lod:1}
-persons.reduce((acc, curr) => { acc[curr.address.city] = (acc[curr.address.city] || 0) + 1; return acc }, {});
+console.log(persons.reduce((acc, curr) => { acc[curr.address.city] = (acc[curr.address.city] || 0) + 1; return acc }, {}));
