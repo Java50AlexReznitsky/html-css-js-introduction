ce525fd87fcc2c9db3d809dfc8d20e27a7c3913c
Barso2 <reznitskyalex@gmail.com>
2023-01-27 16:39:41 +0200

    updated with Hw-21

--- app.js
+++ app.js
@@ -32,6 +32,20 @@ console.log(rectangle1.perimeter());
 HW-21
 Employee  structure and  function createEmployee taken from previous HW
 */
+function createEmployee(id, name, birthYear, salary, city, country) {
+    return { id, name, birthYear, salary, address: { city, country } }
+}
+const employees = [
+    createEmployee(123, "Vasya", 2000, 15000, "Lod", "Israel"),
+    createEmployee(124, "David", 1975, 15500, "Tel-Aviv", "Israel"),
+    createEmployee(125, "Sara", 1985, 20000, "New-York", "USA"),
+    createEmployee(126, "Abraham", 1990, 13000, "London", "UK"),
+    createEmployee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
+    createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "Georgia"),
+    createEmployee(129, "Sasha", 2000, 25000, "Ramat-Gan", "Israel"),
+    createEmployee(130, "Victor", 2003, 10000, "Arad", "Israel")
+]
+
 class Company {
     #employees//object key:<id value>,value:reference to Employee object
     constructor() {
---
+++ test.js
@@ -0,0 +1,3 @@
+const employee1 = {
+    123: { name: "Vasya", birthYear: 2000, salary: 15000, address: { city: "Lod", country: "Israel" } },
+ }
