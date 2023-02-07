541f22908c443a5c8b33e258befe3474c7b65521
Barso2 <reznitskyalex@gmail.com>
2023-02-07 23:33:17 +0200

    CW-24

--- src/main.js
+++ src/main.js
@@ -1,44 +1,11 @@
-import { Company, createEmployee } from "./service/company.js";
-import { EmployeeForm } from "./ui/employee-form.js";
-import { Table } from "./ui/table.js";
-// import { Tabs } from "./ui/tabs.js";
-
-const schema = [
-    { columnName: 'Employee ID', fieldName: 'id' },
-    { columnName: 'Name', fieldName: 'name' },
-    { columnName: 'Birth Year', fieldName: 'birthYear' },
-    { columnName: 'Salary (NIS)', fieldName: 'salary' },
-    { columnName: 'Country', fieldName: 'country' },
-    { columnName: 'City', fieldName: 'city' }
-]
-const company = new Company();
-//HW-23
-////////////////////////////////////////////////////////////////////////////////////////////////////
-const formButtonElement = document.querySelector(".form-button");
-const tableButtonElement = document.querySelector(".table-button");
-// const tab1 = new Tabs();
-// formButtonElement.addEventListener('click',tab1.addEmpl());
-// let employeeForm = {};
-// formButtonElement.addEventListener('click', function () {
-let employeeForm = new EmployeeForm("form-section");
-// });
-// let tableEmployees = {};
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// tableButtonElement.addEventListener('click',function(){
-tableEmployees = new Table("table-section", "Employees", schema);
-// })
-function addEmployee(employeeData) {
-    // console.log(employeeData)
-
-    const employee = createEmployee(employeeData.name,
-        +employeeData.birthYear, +employeeData.salary,
-        employeeData.city, employeeData.country);
-    const res = company.addIdToEmployee(employee);
-    if (!res.message) {
-        employeeData.id = res.id;// employee with id
-        tableEmployees.addRow(employeeData);
-    }
-    return res.message;
-
+import { sleep } from "./utils/sleep.js"
+const TIMEOUT = 5000;
+// const promise = sleep(TIMEOUT)
+// promise.then(() => console.log(`after ${TIMEOUT/1000} seconds`));
+// console.log(`waiting for ${TIMEOUT/1000} seconds`)
+const fun = async () => {
+    console.log(`waiting for ${TIMEOUT / 1000} seconds`);
+    await sleep(TIMEOUT);
+    console.log(`after ${TIMEOUT / 1000} seconds`);
 }
-employeeForm.addFormHandler(addEmployee)
+fun();
