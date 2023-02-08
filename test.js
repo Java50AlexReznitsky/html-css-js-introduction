ed14840f4a3da00af79feb2452361ec121d59c4b
Barso2 <reznitskyalex@gmail.com>
2023-02-08 22:51:59 +0200

    promise exercise

---
+++ test.js
@@ -0,0 +1,37 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// function fun1(a, b) {
+//     return a + b;
+// }
+// function fun2(a, b) {
+//     let res = 0;
+//     for (let i = 1000000000; i > 0; i--) {
+//         res += a * b * i / 3.333 * 6;
+//     }
+//     return res;
+// }
+// function fun3(a, b, c) {
+//     return a + b * c;
+// }
+// console.log("fun1: " + fun1(a, b));
+// console.log('fun2: ' + fun2(c, b));
+// console.log('fun3: ' + fun3(a, b, c));
+
+// let a = 7;
+// // setTimeout(() => {
+// //     a = 99
+// //     console.log(a)
+// // }, 2000);
+// console.log(a)
+
+
+// let b = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         resolve(a = 99);
+//         // console.log(a)
+//     }, 2000);
+// });
+// b.then(function () {
+//     console.log(a);
+// })
