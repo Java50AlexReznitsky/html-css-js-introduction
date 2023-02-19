081bfda14daac74a57a5361321df914b098a48e6
Barso2 <reznitskyalex@gmail.com>
2023-02-19 23:27:27 +0200

    calculator update

--- js/script.js
+++ js/script.js
@@ -6,41 +6,62 @@ const minusBtn = document.querySelector('.minus-btn');
 const divideBtn = document.querySelector('.divide-btn');
 const multiplyBtn = document.querySelector('.multiply-btn');
 const section1 = document.querySelector('.section1');
+const buttons = document.querySelectorAll("button")
 const a = +input1.value
 const b = +input2.value
+function deactivateButtons() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.add("is-not-active")
+    }
+}
+
 
 plusBtn.onclick = () => {
+    deactivateButtons()
+    plusBtn.classList.remove('is-not-active')
+    plusBtn.classList.add('is-active')
     section1.innerHTML = ''
     section1.hidden = false
     reset.hidden = false
-    section1.innerHTML += `${input1.value} + ${input2.value} = ${+input1.value + +input2.value}<br>`
+    section1.innerHTML += `${input1.value} + ${input2.value} = ${+input1.value + +input2.value}`
 };
 minusBtn.onclick = () => {
+    deactivateButtons()
+    minusBtn.classList.remove('is-not-active')
+    minusBtn.classList.add('is-active')
     section1.innerHTML = ''
     section1.hidden = false
     reset.hidden = false
-    section1.innerHTML += `${input1.value} - ${input2.value} = ${+input1.value - +input2.value}<br>`
+    section1.innerHTML += `${input1.value} - ${input2.value} = ${+input1.value - +input2.value}`
     // input1.value = ''
 };
 divideBtn.onclick = () => {
+    deactivateButtons()
+    divideBtn.classList.remove('is-not-active')
+    divideBtn.classList.add('is-active')
     section1.innerHTML = ''
     section1.hidden = false
     reset.hidden = false
-    section1.innerHTML += `${input1.value} / ${input2.value} = ${+input1.value / +input2.value}<br>`
+    section1.innerHTML += `${input1.value} / ${input2.value} = ${+input1.value / +input2.value}`
     // input1.value = ''
 };
 multiplyBtn.onclick = () => {
+    deactivateButtons()
+    multiplyBtn.classList.remove('is-not-active')
+    multiplyBtn.classList.add('is-active')
     section1.innerHTML = ''
     section1.hidden = false
     reset.hidden = false
-    section1.innerHTML += `${input1.value} * ${input2.value} = ${+input1.value * +input2.value}<br>`
+    section1.innerHTML += `${input1.value} * ${input2.value} = ${+input1.value * +input2.value}`
     // input1.value = ''
 };
 reset.onclick = () => {
+    deactivateButtons()
     section1.innerHTML = ''
     section1.hidden = true
     input1.value = ''
     input2.value = ''
     reset.hidden = true
+    // plusBtn.classList.remove('is-active')
 
 }
