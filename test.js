8094d402693bf4c760b86c814e02239acfe3f076
Barso2 <reznitskyalex@gmail.com>
2023-02-22 01:06:59 +0200

    spread - rest

---
+++ test.js
@@ -0,0 +1,9 @@
+//1.concat()
+// const arr1 = ["Sasha","Sveta","Maxim","Alisa"]
+// const arr2 = ["Boba"]
+// const family = arr1.concat(arr2)
+// console.log(family)
+//2. rest-spread
+// const arr3 = ["Apple","Banana","Ananas","Grape"]
+// console.log(arr3.copyWithin(3,0))
+// console.log(arr3)
