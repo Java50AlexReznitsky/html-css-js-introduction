8db8562cffec661068b5891dc1cb7f4c6c219a14
Barso2 <reznitskyalex@gmail.com>
2023-02-22 04:07:56 +0200

    selfEmployee application start

--- js/script.js
+++ js/script.js
@@ -1,67 +1,39 @@
-const input1 = document.querySelector('.input1');
-const input2 = document.querySelector('.input2');
-const reset = document.querySelector('.reset');
-const plusBtn = document.querySelector('.plus-btn');
-const minusBtn = document.querySelector('.minus-btn');
-const divideBtn = document.querySelector('.divide-btn');
-const multiplyBtn = document.querySelector('.multiply-btn');
-const section1 = document.querySelector('.section1');
-const buttons = document.querySelectorAll("button")
-const a = +input1.value
-const b = +input2.value
-function deactivateButtons() {
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.add("is-not-active")
-    }
+const divElement = document.querySelector('.test')
+const showButton = document.querySelector('.show')
+const hideButton = document.querySelector('.hide')
+const divsArr = ["Hello", "how", "are", "you", "today", "?"]
+divsArr.forEach(item => {
+    divElement.innerHTML += `<p class="par hidden">${item}</p>`
+})
+const parClasses = document.querySelectorAll('.par');
+showButton.addEventListener("click", showText);
+hideButton.addEventListener("click", hideText);
+// showButton.onclick = () => { showText() }
+// hideButton.onclick = () => { hideText() }
+
+function showText() {
+    parClasses.forEach(item => {
+        item.classList.add("is-shown")
+        item.classList.remove("hidden")
+    })
 }
-
-
-plusBtn.onclick = () => {
-    deactivateButtons()
-    plusBtn.classList.remove('is-not-active')
-    plusBtn.classList.add('is-active')
-    section1.innerHTML = ''
-    section1.hidden = false
-    reset.hidden = false
-    section1.innerHTML += `${input1.value} + ${input2.value} = ${+input1.value + +input2.value}`
-};
-minusBtn.onclick = () => {
-    deactivateButtons()
-    minusBtn.classList.remove('is-not-active')
-    minusBtn.classList.add('is-active')
-    section1.innerHTML = ''
-    section1.hidden = false
-    reset.hidden = false
-    section1.innerHTML += `${input1.value} - ${input2.value} = ${+input1.value - +input2.value}`
-    // input1.value = ''
-};
-divideBtn.onclick = () => {
-    deactivateButtons()
-    divideBtn.classList.remove('is-not-active')
-    divideBtn.classList.add('is-active')
-    section1.innerHTML = ''
-    section1.hidden = false
-    reset.hidden = false
-    section1.innerHTML += `${input1.value} / ${input2.value} = ${+input1.value / +input2.value}`
-    // input1.value = ''
-};
-multiplyBtn.onclick = () => {
-    deactivateButtons()
-    multiplyBtn.classList.remove('is-not-active')
-    multiplyBtn.classList.add('is-active')
-    section1.innerHTML = ''
-    section1.hidden = false
-    reset.hidden = false
-    section1.innerHTML += `${input1.value} * ${input2.value} = ${+input1.value * +input2.value}`
-    // input1.value = ''
-};
-reset.onclick = () => {
-    deactivateButtons()
-    section1.innerHTML = ''
-    section1.hidden = true
-    input1.value = ''
-    input2.value = ''
-    reset.hidden = true
-    // plusBtn.classList.remove('is-active')
-
+function hideText() {
+    parClasses.forEach(item => {
+        item.classList.add("hidden")
+        item.classList.remove("is-shown")
+    })
 }
+// function setActive() {
+//     parClasses.forEach(item => {
+//         item.classList.add("is-active")
+//     })
+// }
+// function setNotActive() {
+//     parClasses.forEach(item => {
+//         item.classList.remove("is-active")
+//     })
+// }
+
+// for (let i = 0; i < divsArr.length; i++) {
+//     divElement.innerHTML += `<p class="par">${divsArr[i]}</p>`
+// }
